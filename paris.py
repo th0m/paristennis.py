53883dfa26d5057f0d9743a423bc8902b28f8627
Thomas Lefebvre <thomas.lefebvre3@gmail.com>
2014-12-08 18:27:16 +0100

    Get number of pages for each day

--- paris.py
+++ paris.py
@@ -4,8 +4,8 @@ import requests
 
 def login(s, login, password):
     s.get('https://teleservices.paris.fr/srtm/jsp/web/index.jsp')
-    payload = {'login': login, 'password': password}
-    s.post('https://teleservices.paris.fr/srtm/authentificationConnexion.action', data=payload)
+    data = {'login': login, 'password': password}
+    s.post('https://teleservices.paris.fr/srtm/authentificationConnexion.action', data=data)
 
 def getInfos(s):
     r = s.get('https://teleservices.paris.fr/srtm/compteConsulterDonneePersoInit.action')
@@ -13,7 +13,7 @@ def getInfos(s):
 def getTennisList(s):
     r = s.get('https://teleservices.paris.fr/srtm/reservationCreneauInit.action')
     soup = BeautifulSoup.BeautifulSoup(r.text)
-    tennis = soup.find(attrs={"name": 'tennisArrond'})
+    tennis = soup.find(attrs={'name': 'tennisArrond'})
     tlist = []
     for tennis in tennis.findAll('option'):
         split = tennis['value'].split('@',1) 
@@ -21,15 +21,34 @@ def getTennisList(s):
             tlist.append({'name': split[0], 'arrdt': split[1]})
     return tlist
 
-def getDisponibilities(s):
-    r = s.get('https://teleservices.paris.fr/srtm/reservationCreneauInit.action')
-    print(r.text)
+def crawl(s, tousArrondissements, courtCouvert):
+    data = {
+        'actionInterne': 'recherche',
+        'provenanceCriteres': 'true',
+    }
+
+    if tousArrondissements:
+        data['tousArrondissements'] = 'on'
+
+    if courtCouvert:
+        data['courtCouvert'] = 'on'
+
+    data['dateDispo'] = '09/12/2014'
+    for heureDispo in range(8, 22):
+        data['heureDispo'] = heureDispo
+        r = s.post('https://teleservices.paris.fr/srtm/reservationCreneauListe.action', data=data)
+        soup = BeautifulSoup.BeautifulSoup(r.text)
+        pagelinks = soup.find(attrs={'class': 'pagelinks'}).findAll('a')
+        if len(pagelinks) > 0:
+            print(data['dateDispo']+' at '+str(heureDispo)+' '+pagelinks[-1]['href'].split('d-41734-p=')[1].split('&')[0])
+        else:
+            print(data['dateDispo']+' at '+str(heureDispo)+' 1')
 
 def main():
     s = requests.Session()
     login(s, '171091026', '5434')
     #getInfos(s)
     #getTennisList(s)
-    getDisponibilities(s)
+    crawl(s, True, False)
 
 main()
